ddd1eed222a1a2e02d9dfe32c750cc8f0b3911ea
username <youremail@domain.com>
2023-03-06 09:32:26 -0500

    setup boilerplate

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Pseudocode
+
+// Pseudocode outlines the app's logic using plain language. 
+// It provides a road map to writing the code itself.
+
+
+
+
+// STATE/DATA 
+// We will be writing the program's state to set up the basics of out game
+// // What does the application need to "remember" throughout its execution?
+
+function inIt (){
+    we need to remember the scores of the player and the computer
+
+    we need to have a balance 
+
+balance = {
+    player: 0  //we need to be able to update this balance in two ways 
+    //by adding funds, by withdrawing, and by wins (if we match a line of 3 or five)
+}
+
+
+
+
+winner = null; 
+//the winner is null
+//if computer wins we keep the funds, if player wins we add to b
+
+
+
+}
+
+
+
+
+
+
+
+//RENDER
+// we will be using the render function to update the data as the player bets
+
+// we need it to update 
+// player's balance : 
+// if wins or loses 
+
+//  we will start with $5 bets! if you think you can handle a bigger challenge, 
+//  we can add different betting ammounts for different winning options
+
+
+
+
+// THE DOM (DOCUMENT OBJECT MODEL)
+// the dom will allow us to update the HTML visually as we play the game
+// we will do so using CSS 
