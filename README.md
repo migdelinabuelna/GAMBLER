5080ef05f7b2eb9e2ba5d0c3433ed480e8470e14
username <youremail@domain.com>
2023-03-09 20:54:43 -0500

    README UPDATE

--- README.md
+++ README.md
@@ -20,12 +20,14 @@ In the HTML tag we added a title, linked CSS & JavaScript to work with them in s
 ## JAVASCRIPT 
 
 
-### function slotImages 
+### slotImages array
+/*----- constants -----*/
 Create a slotImages array. It includes three images: skull.png which was the index of 0
 flower.png which has the index of 1,
 and heart.png with an index of 2.
 
 ### function getRandomImages
+/*----- function -----*/
 First function in JavaScript. Within the function, we are using the Math.random() method which is set to generate a random number from 0 through <1. We are adjusting the method by adding three (Math.random() *3) which then will generate random numbers from 0 through <3. This method gives us decimals, and to eliminate that, we added Math.floor to the method, which will round the random numbers to the lowest whole number being 0 through <3. 
 The function is then asked to return slotImages and display the index that matches with the randomSpin number. 
 
@@ -42,10 +44,63 @@ you can console.log the function as follows for more visual details.
 console.log(getRandomImages()); 
 
 ### addEventListener
-
+ /*----- cached element -----*/
 Within our HTML we have the <button> tag with the ID of #betting. We access that element in JS using the ID and we assign it the new variable 'button'. 
+
+/*----- event listeners -----*/
 We then use the button variable and we add the event listener to it. The event listener will wait for a click and when it happens it will call on the playSlotMachine function. 
 
 
-### function playSlotMachine
+### display object
+/*----- state variables -----*/
+We created an object with three keys: a, b, and c. 
 
+
+### function playSlotMachine
+ /*----- function -----*/
+We start by selecting our <main> tag using the #photos id that is attached to it. We then assign it an empty string to reset the element content to blank each time the function starts running. 
+
+
+Next, we proceed with our slots. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sellect all of the elements by class name using queryselectorall
+
+
+
+    //we want to see if each of the elements source property are equal to eachother
+
+
+
+
+
+ // console.log(slots) // <--this should show the different pictures with the keys a, b, and c as asigned above
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 
+
 // Create array. The indexes: 0= skull.png. 1= flower.png, 2= heart.png
 const slotImages = [ 
     "imgs/skull.png",
@@ -16,65 +17,52 @@ function getRandomImages(){
 
 
 
- /*----- event listeners -----*/
 // Runs function playSlotMachine when 'button' is clicked.
-const button = document.getElementById('betting'); //asigning new variable 
+const button = document.getElementById('betting');//asigning new variable  
 button.addEventListener('click', playSlotMachine);
 
 
-let slots = { // we created an object here to identify the image that gets shuffled randomly 
+let display = { 
     a: "",
     b: "",
     c: ""
 }
 
- 
-/*----- functions -----*/
+
 
 function playSlotMachine() {
-// we are selecting the photos id and assigning it to the picture element
+
 // const picture = document.querySelector('#photos') 
 // picture.innerHTML = "" // empty string to clear out the main tag of all the image tags so that evey time you click the images reset 
 
-document.getElementById('photos').innerHTML = "" // <-- this is the same! why are we assining a new value photos?
-
-// document.querySelector('#photos') we are selecting the element with the id photos 
+document.getElementById('photos').innerHTML = "" // this resets the main tag of all it's elements as the function starts
 
 
-//is there another name to refer to these blocks?
-
-const skull = document.createElement('img')
-skull.id = 'skull'
-slots.a = getRandomImages(); //<-we are calling the get random images function - use () when calling a functio
-skull.src = slots.a
-document.getElementById('photos').appendChild(skull)
+const firstslot = document.createElement('img')
+display.a = getRandomImages(); //<-we are calling the get random images function - use () when calling a functio
+firstslot.src = display.a //changing the source of the image tag 
+document.getElementById('photos').appendChild(firstslot) 
 
 
-const flower =  document.createElement('img')
-flower.id = 'flower'
-slots.b = getRandomImages(); //We are assigning the flower value // <-calling the funciton
-flower.src = slots.b
-document.getElementById('photos').appendChild(flower)
+const secondslot =  document.createElement('img')
+display.b = getRandomImages();
+secondslot.src = display.b
+document.getElementById('photos').appendChild(secondslot)
 
 
-const heart = document.createElement('img') //-> creating an img element that will go inside the <main> tag
-//give the element an id
-heart.id = 'heart' // these can have the same class name so you can group them together later 
-slots.c = getRandomImages(); // assign image- call the function for random img 
-heart.src = slots.c
-document.getElementById('photos').appendChild(heart)// now select main tag by id name and assign the heart element to it 
+const thirdslot = document.createElement('img') //-> creating an img element that will go inside the <main> tag 
+display.c = getRandomImages(); // assign image- call the function for random img 
+thirdslot.src = display.c
+document.getElementById('photos').appendChild(thirdslot)// now select main tag by id name and assign the heart element to it 
 
 displayMessage() //--> this is calling on the next function to run 
 
 }
 
 
-
 function displayMessage() {
-    // sellect all of the elements by class name using queryselectorall 
-    //we want to see if each of the elements source property are equal to eachother
-
-  if (slots.a === slots.b && slots.b === slots.c) { 
+     
+  if (display.a === display.b && display.b === display.c) { 
     document.getElementById('message').innerHTML = ('$$$ Win Win Win $$$'); //we are targetting the message id within the header tag to then display Win Win Win inside HTML
     document.getElementById('beat').play(); //<this is identifying your audio tag in HTML with the beat id and is is instructing to play the audio if condition it met
 } else  { 
@@ -84,18 +72,10 @@ function displayMessage() {
 
  }
 
-    // console.log(slots) // <--this should show the different pictures with the keys a, b, and c as asigned above
-    
+   
 
 
-
-/*----- constants -----*/
-
-
-  /*----- state variables -----*/
-
-
-  /*----- cached elements  -----*/
+  
 
 
  
